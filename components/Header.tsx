0b80f4757c5e85c82570c05f66676b927435daa6
mkError420 <mk.rabbani.cse@gmail.com>
2026-02-22 12:10:56 +0600

    navbar responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,13 +13,14 @@ const Header = () => {
     <header className='bg-white py-5 sticky top-0 z-40 w-full border-b border-gray-100' >
       <Container className='flex items-center justify-between text-lightColor'> 
         {/* Logo */}
-        <div className='w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0 '>
+        <div className='flex items-center gap-2.5 justify-start md:gap-0 md:w-1/3 lg:w-1/4 w-1/2'>
           <MobileMenu/>
           <Logo/>
         </div>
         {/* Navbutton */}
         <HeaderMenu/>
-        <div className='w-auto md:w-1/3 flex items-center justify-end gap-5 '>
+        {/* Right Section */}
+        <div className='flex items-center justify-end gap-2 md:gap-3 lg:gap-5 md:w-1/3 lg:w-1/4 w-1/2'>
           <SearchBar/>
           <CartIcon/>
           <FavoriteButton/>
--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -8,10 +8,10 @@ function HeaderMenu() {
   const pathname = usePathname()
   
   return (
-    <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor '>
+    <div className='hidden md:flex md:w-1/3 lg:w-1/2 items-center justify-center gap-2 md:gap-3 lg:gap-6 text-sm capitalize font-semibold text-lightColor '>
       {headerData?.map((item)=>(
         <Link key={item?.title} href={item?.link} className=
-        {`hover:text-shop_light_green hoverEffect relative group ${pathname === item?.link && "text-shop_light_green" }`}>
+        {`hover:text-shop_light_green hoverEffect relative group whitespace-nowrap ${pathname === item?.link && "text-shop_light_green" }`}>
           {item?.title}
           <span className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:left-0 ${
             pathname === item?.link && "w-1/2"
